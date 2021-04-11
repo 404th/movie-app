ea50806ab7b7839af0891c3efbd2b9d516d2b322
= <=>
2021-04-11 19:09:03 +0500

    Footer part of project is ready

--- client/src/Components/Footer/footer.jsx
+++ client/src/Components/Footer/footer.jsx
@@ -1,9 +1,34 @@
-
+import "./style/footerStyle.css"
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer_container">
+      <div className="footer_container_brand">
+        <img src="./img/UMovies.svg" alt="UMovies Brand" width="166px" height="41px" />
+        <p>Designed and Copyrights By <span>Jayprakash Kachare</span></p>
+        <div className="footer_container_brand_social">
+          <Link to={ "#" } className="fa fa-facebook"></Link>
+          <Link to={ "#" } className="fa fa-twitter"></Link>
+          <Link to={ "#" } className="fa fa-instagram"></Link>
+          <Link to={ "#" } className="fa fa-linkedin"></Link>
+        </div>
+      </div>
+      <div className="footer_container_quick">
+        <span>QUICK LINKS</span>
+        <Link className="footer_container_quick_link" to={ "#" }>Movies</Link>
+        <Link className="footer_container_quick_link" to={ "#" }>Actors</Link>
+        <Link className="footer_container_quick_link" to={ "#" }>Trailers</Link>
+        <Link className="footer_container_quick_link" to={ "#" }>About</Link>
+        <Link className="footer_container_quick_link" to={ "#" }>Privacy and Policy</Link>
+      </div>
+      <div className="footer_container_newsletter">
+        <span className="footer_container_newsletter_title">NEWSLETTER</span>
+        <div className="footer_container_newsletter_div">
+          <input className="footer_container_newsletter_div_input" placeholder="Newsletter signup" type="text"/>
+          <button className="footer_container_newsletter_div_button">SIGN UP</button>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/Components/Slider/slider.jsx
+++ client/src/Components/Slider/slider.jsx
@@ -5,14 +5,51 @@ import "slick-carousel/slick/slick-theme.css";
 
 import SliderItem from './SliderItem/sliderItem'
 
+
 function SimpleSlider() {
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 1
-  };
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1720,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
   return (
     <Slider className="slider_container" {...settings}>
       <SliderItem />
